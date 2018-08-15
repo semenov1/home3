531d24e8e355716f90e5a3aa514c143ac535da3f
Danil Semenov <semenov.danila999@.gmail.com>
2018-08-15 12:02:03 +0300

    home3

---
+++ js/home3.js
@@ -0,0 +1,10 @@
+let number =prompt("Enter the number,please","");
+number = Number(number);
+
+ let factorial=1;
+for(let i = 1; i <=number; i++){ 
+    factorial*=i;
+}
+console.log(factorial);
+
+
